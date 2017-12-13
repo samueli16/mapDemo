1f78462e4697668337c4b104a72c65a85341e1a2
bjm5672001 <33645668+bjm5672001@users.noreply.github.com>
2017-12-13 15:17:28 -0700

    new version

---
+++ informationBox.js
@@ -0,0 +1,500 @@
+info = [
+	{
+		"countryId": "United States of America",
+		"anchorLat": "38.897957",
+		"anchorLng": "-77.036560",
+		"countryNum": "0",
+		"countryImage": "CountryImages/USA.png",
+		"numOfAircraft": "0",
+		"numOfBases": "0",
+		"bases": null,
+		"population": "323.1 million",
+		"capital": "Washington DC",
+		"languageOfficial": "English",
+		"currency": "United States Dollar (USD)",
+		"numOfFamilyUnits": "1",
+		"familyUnits":
+			[
+				{
+					"lastName": "Lind",
+					"displayName": "Matthew and Lisa Lind",
+					"photo": "MissionaryPhotos/USA/Lind.jpg",
+					"donationLink": "https://www.maf.org/lind#bodycontent5",
+					"pageLink": "https://www.maf.org/lind",
+					"hasBlog": true,
+					"blogLink": "https://lindfamily.org/",
+					"preField": false
+				},
+				{
+					"lastName": "Abell",
+					"displayName":"Jim and Candace Abell",
+					"photo": "https://www.maf.org/image/missionaries/abell.jpg",
+					"donationLink": "https://www.maf.org/abell#bodycontent5",
+					"pageLink": "https://www.maf.org/abell",
+					"hasBlog": false,
+					"blogLink": null,
+					"prefield": false
+				},
+				{
+					"lastName": "Appleby",
+					"displayName":"Dan and Jodi Appleby",
+					"photo": "https://www.maf.org/image/6.-missionary-pages/Appleby.jpg",
+					"donationLink": "https://www.maf.org/appleby#bodycontent5",
+					"pageLink": "https://www.maf.org/appleby",
+					"hasBlog": false,
+					"blogLink": null,
+					"prefield": false
+				},
+				{
+					"lastName": "Arnold",
+					"displayName":"Gene and Jennifer Arnold",
+					"photo": "https://www.maf.org/image/6.-missionary-pages/arnold-2.png",
+					"donationLink": "https://www.maf.org/arnold#bodycontent5",
+					"pageLink": "https://www.maf.org/arnold",
+					"hasBlog": false,
+					"blogLink": null,
+					"prefield": false
+				},
+
+			]
+	},
+	{
+		"countryId": "Suriname",
+		"anchorLat": "5.839398",
+		"anchorLng": "-55.199089",
+		"countryNum": "1",
+		"countryImage": "CountryImages/Suriname.png",
+		"numOfAircraft": "2",
+		"numOfBases": "1",
+		"bases":
+			[
+				{ "baseId": "Paramaribo" }
+			],
+		"population": "543,000",
+		"capital": "Paramaribo",
+		"languageOfficial": "Dutch",
+		"currency": "Surinamese dollar (SRD)",
+		"numOfFamilyUnits": "1",
+		"familyUnits":
+			[
+				{
+					"lastName": "Portillo",
+					"displayName": "Juan Carlos and Johanna Portillo",
+					"photo": "MissionaryPhotos/Suriname/Portillo.jpg",
+					"donationLink": "https://www.maf.org/portillo#bodycontent5",
+					"pageLink": "https://www.maf.org/portillo",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+			]
+	},
+	{
+		"countryId": "Brazil",
+		"anchorLat": "-15.7942",
+		"anchorLng": "-47.8825",
+		"countryNum": "2",
+		"countryImage": "CountryImages/Brazil.png",
+		"numOfAircraft": "0",
+		"numOfBases": "1",
+		"bases":
+			[
+				{ "baseId": "Asas de Socorro" }
+			],
+		"population": "202,700,000",
+		"capital": "Brasilia",
+		"languageOfficial": "Portuguese",
+		"currency": "Real (BRL)",
+		"numOfFamilyUnits": "1",
+		"familyUnits":
+			[
+				{
+					"lastName": "Ault",
+					"displayName": "Tim and Roxanne Ault",
+					"photo": "MissionaryPhotos/Brazil/Ault.jpg",
+					"donationLink": "https://www.maf.org/ault#bodycontent5",
+					"pageLink": "https://www.maf.org/ault",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+			]
+
+	},
+	{
+		"countryId": "Costa Rica",
+		"anchorLat": "9.9281",
+		"anchorLng": "-84.0907",
+		"countryNum": "3",
+		"countryImage": "CountryImages/CostaRica.png",
+		"numOfAircraft": "0",
+		"numOfBases": "1",
+		"bases":
+			[
+				{ "baseId": "San Jose" }
+			],
+		"population": "4,600,000",
+		"capital": "San Jose",
+		"languageOfficial": "Spanish",
+		"currency": "Costa Rican Colón (C)",
+		"numOfFamilyUnits": "2",
+		"familyUnits":
+			[
+				{
+					"lastName": "Sanchez",
+					"displayName": "Mauricio and Rosalia Sanchez",
+					"photo": "MissionaryPhotos/CostaRica/Sanchez.jpg",
+					"donationLink": "https://www.maf.org/sanchez#bodycontent5",
+					"pageLink": "https://www.maf.org/sanchez",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Rhoads",
+					"displayName": "Brad and Susan Rhoads",
+					"photo": "MissionaryPhotos/CostaRica/Rhoads.jpg",
+					"donationLink": "https://www.maf.org/rhoads#bodycontent5",
+					"pageLink": "https://www.maf.org/rhoads",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+
+			]
+	},
+	{
+		"countryId": "Democratic Republic of the Congo",
+		"anchorLat": "-4.322447",
+		"anchorLng": "15.307045",
+		"countryNum": "4",
+		"countryImage": "CountryImages/DRC.png",
+		"numOfAircraft": "9",
+		"numOfBases": "4",
+		"bases":
+			[
+				{ "baseId": "Kinshasa" },
+				{ "baseId": "Lubumbashi"},
+				{ "baseId": "Bunia"},
+				{ "baseId": "Nyankundé"}
+			],
+		"population": "77.4 million",
+		"capital": "Kinshasa",
+		"languageOfficial": "French",
+		"currency": "Congolese Franc (CDF)",
+		"numOfFamilyUnits": "16",
+		"familyUnits":
+			[
+				{
+					"lastName": "Hochstetler",
+					"displayName": "Rod and Valerie Hochstetler",
+					"photo": "MissionaryPhotos/DRC/Hochstetler.jpg",
+					"donationLink": "https://www.maf.org/hochstetler#bodycontent5",
+					"pageLink": "https://www.maf.org/hochstetler",
+					"hasBlog": true,
+					"blogLink": "http://hochstetlerheirways.blogspot.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Jacobsson",
+					"displayName": "David and Donna Jacobsson",
+					"photo": "MissionaryPhotos/DRC/Jacobsson.jpg",
+					"donationLink": "https://www.maf.org/jacobsson#bodycontent5",
+					"pageLink": "https://www.maf.org/jacobsson",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Birkemo",
+					"displayName": "Nate and Terra Birkemo",
+					"photo": "MissionaryPhotos/DRC/Birkemo.jpg",
+					"donationLink": "https://www.maf.org/birkemo#bodycontent5",
+					"pageLink": "https://www.maf.org/birkemo",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Paul",
+					"displayName": "Chase and Gwendolyn Paul",
+					"photo": "MissionaryPhotos/DRC/Paul.jpg",
+					"donationLink": "https://www.maf.org/paul#bodycontent5",
+					"pageLink": "https://www.maf.org/paul",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Hale",
+					"displayName": "Stephen and Ruth Hale",
+					"photo": "MissionaryPhotos/DRC/Hale.jpg",
+					"donationLink": "https://www.maf.org/hale#bodycontent5",
+					"pageLink": "https://www.maf.org/hale",
+					"hasBlog": true,
+					"blogLink": "https://stephenandruth.wordpress.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Shutts",
+					"displayName": "Mike and Sharol Shutts",
+					"photo": "MissionaryPhotos/DRC/Shutts.jpg",
+					"donationLink": "https://www.maf.org/shutts#bodycontent5",
+					"pageLink": "https://www.maf.org/shutts",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Harkonen",
+					"displayName": "Timo and Laura Harkonen",
+					"photo": "MissionaryPhotos/DRC/Harkonen.jpg",
+					"donationLink": "https://www.maf.org/harkonen#bodycontent5",
+					"pageLink": "https://www.maf.org/harkonen",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Cadd",
+					"displayName": "Jon and Cher Cadd",
+					"photo": "MissionaryPhotos/DRC/Cadd.jpg",
+					"donationLink": "https://www.maf.org/cadd#bodycontent5",
+					"pageLink": "https://www.maf.org/cadd",
+					"hasBlog": true,
+					"blogLink": "https://captainsblogafrica.wordpress.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Pedersoon",
+					"displayName": "Garth and Jody Pederson",
+					"photo": "MissionaryPhotos/DRC/Pederson.jpg",
+					"donationLink": "https://www.maf.org/pederson#bodycontent5",
+					"pageLink": "https://www.maf.org/pederson",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "De Jongh",
+					"displayName": "Jonathan and Nicole De Jongh",
+					"photo": "MissionaryPhotos/DRC/DeJongh.jpg",
+					"donationLink": "https://www.maf.org/dejongh#bodycontent5",
+					"pageLink": "https://www.maf.org/dejongh",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Martin",
+					"displayName": "Joey and Kathleen Martin",
+					"photo": "MissionaryPhotos/DRC/Martin.jpg",
+					"donationLink": "https://www.maf.org/jmartin#bodycontent5",
+					"pageLink": "https://www.maf.org/jmartin",
+					"hasBlog": true,
+					"blogLink": "http://joeyandkathleen.blogspot.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Dimon",
+					"displayName": "Chad and Jamie Dimon",
+					"photo": "MissionaryPhotos/DRC/Dimon.jpg",
+					"donationLink": "https://www.maf.org/dimon#bodycontent5",
+					"pageLink": "https://www.maf.org/dimon",
+					"hasBlog": true,
+					"blogLink": "https://dimonsintherough.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Spann",
+					"displayName": "Kevin and Tasha Spann",
+					"photo": "MissionaryPhotos/DRC/Spann.jpg",
+					"donationLink": "https://www.maf.org/spann#bodycontent5",
+					"pageLink": "https://www.maf.org/spann",
+					"hasBlog": true,
+					"blogLink": "http://spann-ingtheglobe.blogspot.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Strietzel",
+					"displayName": "Lary and Sheryl Strietzel",
+					"photo": "MissionaryPhotos/DRC/Strietzel.jpg",
+					"donationLink": "https://www.maf.org/strietzel#bodycontent5",
+					"pageLink": "https://www.maf.org/strietzel",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Petersen",
+					"displayName": "David and Ashley Petersen",
+					"photo": "MissionaryPhotos/DRC/Petersen.jpg",
+					"donationLink": "https://www.maf.org/petersen#bodycontent5",
+					"pageLink": "https://www.maf.org/petersen",
+					"hasBlog": true,
+					"blogLink": "https://davidashleypetersen.wordpress.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Mbodwam",
+					"displayName": "Emmanuel and Kahindo Mbodwam",
+					"photo": "MissionaryPhotos/DRC/Mbodwam.jpg",
+					"donationLink": "https://www.maf.org/mbodwam#bodycontent5",
+					"pageLink": "https://www.maf.org/mbodwam",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+
+			]
+	},
+	{
+		"countryId": "Ecuador",
+		"anchorLat": "-0.180653",
+		"anchorLng": "-0.180653",
+		"countryNum": "5",
+		"countryImage": "CountryImages/Ecuador.png",
+		"numOfAircraft": "0",
+		"numOfBases": "2",
+		"bases":
+			[
+				{ "baseId": "Quito" },
+				{ "baseId": "Shell"}
+			],
+		"population": "15.7 million",
+		"capital": "Quito",
+		"languageOfficial": "Spanish",
+		"currency": "Dollarized",
+		"numOfFamilyUnits": "2",
+		"familyUnits":
+			[
+				{
+					"lastName": "Souza",
+					"displayName": "Marcos and Maddison Souza",
+					"photo": "MissionaryPhotos/Ecuador/Souza.jpg",
+					"donationLink": "https://www.maf.org/souza#bodycontent5",
+					"pageLink": "https://www.maf.org/souza",
+					"hasBlog": true,
+					"blogLink": "https://marcosandmaddie.com/",
+					"preField": false
+				},
+				{
+					"lastName": "Whitehead",
+					"displayName": "Dan and Tracey Whitehead",
+					"photo": "MissionaryPhotos/Ecuador/Whitehead.jpg",
+					"donationLink": "https://www.maf.org/whitehead#bodycontent5",
+					"pageLink": "https://www.maf.org/whitehead",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+
+			]
+	},
+	{
+		"countryId": "Mexico",
+		"anchorLat": "19.42847",
+		"anchorLng": " -99.12766",
+		"countryNum": "6",
+		"countryImage": "CountryImages/Mexico.png",
+		"numOfAircraft": "2",
+		"numOfBases": "1",
+		"bases":
+			[
+				{ "baseId": "Oaxaca" },
+			],
+		"population": "113.7 million",
+		"capital": "Mexico City",
+		"languageOfficial": "Spanish",
+		"currency": "Mexican Peso (MXN)",
+		"numOfFamilyUnits": "2",
+		"familyUnits":
+			[
+				{
+					"lastName": "Ross",
+					"displayName": "Mike and Joan Ross",
+					"photo": "MissionaryPhotos/Mexico/Ross.jpg",
+					"donationLink": "https://www.maf.org/ross#bodycontent5",
+					"pageLink": "https://www.maf.org/ross",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Weatherstone",
+					"displayName": "Chuck and Susan Weatherstone",
+					"photo": "MissionaryPhotos/Mexico/Weatherstone.jpg",
+					"donationLink": "https://www.maf.org/weatherstone#bodycontent5",
+					"pageLink": "https://www.maf.org/weatherstone",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+
+			]
+	},
+	{
+		"countryId": "Hati",
+		"anchorLat": "18.575394",
+		"anchorLng": " -72.294708",
+		"countryNum": "7",
+		"countryImage": "CountryImages/Hati.png",
+		"numOfAircraft": "3",
+		"numOfBases": "1",
+		"bases":
+			[
+				{ "baseId": "Port Au Prince" },
+			],
+		"population": "9.7 million",
+		"capital": "Port Au Prince",
+		"languageOfficial": "Haitian Kreyol, French",
+		"currency": "Gourde (HTG)",
+		"numOfFamilyUnits": "4",
+		"familyUnits":
+			[
+				{
+					"lastName": "Carwell",
+					"displayName": "David and Patricia Carwell",
+					"photo": "MissionaryPhotos/Hati/Carwell.jpg",
+					"donationLink": "https://www.maf.org/carwell#bodycontent5",
+					"pageLink": "https://www.maf.org/carwell",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Simon",
+					"displayName": "David and Erin Simon",
+					"photo": "MissionaryPhotos/Hati/Simon.jpg",
+					"donationLink": "https://www.maf.org/simon#bodycontent5",
+					"pageLink": "https://www.maf.org/simon",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Weatherstone",
+					"displayName": "Chuck and Susan Weatherstone",
+					"photo": "MissionaryPhotos/Mexico/Weatherstone.jpg",
+					"donationLink": "https://www.maf.org/weatherstone#bodycontent5",
+					"pageLink": "https://www.maf.org/weatherstone",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				},
+				{
+					"lastName": "Weatherstone",
+					"displayName": "Chuck and Susan Weatherstone",
+					"photo": "MissionaryPhotos/Mexico/Weatherstone.jpg",
+					"donationLink": "https://www.maf.org/weatherstone#bodycontent5",
+					"pageLink": "https://www.maf.org/weatherstone",
+					"hasBlog": false,
+					"blogLink": null,
+					"preField": false
+				}
+
+			]
+	}
+
+
+];
